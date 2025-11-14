0e4a27a55ec2436c0cda8a98ffdb9335522fe335
Hamza Güleşci <hamza.gulesci2008@gmail.com>
2025-11-14 17:47:01 +0300

    adsensefixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="tr">
-      <head></head>
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3164898310826304"
+             crossOrigin="anonymous"></script>
+      </head>
       <body className={inter.className}>
         {children}
         <Toaster position="top-center" richColors closeButton />
